12e1bd6def5372563f9b12fb846330f180b689fe
Yinon bar <yinonbar1988@gmail.com>
2023-02-24 10:27:24 +0200

    clean slate

--- mrdigital-theme/functions.php
+++ mrdigital-theme/functions.php
@@ -3,7 +3,8 @@
 function loadScripts()
 {
   wp_enqueue_style("bootstrap", "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css");
-  wp_enqueue_style("mainStyle", get_stylesheet_uri());
+  wp_enqueue_style("mainStyle", get_theme_file_uri() . "/css/main.css");
+  wp_enqueue_script('jquery');
   wp_enqueue_script("bootstrapJS", "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js", 'jquery', false, true);
 }
 
---
+++ mrdigital-theme/template-contactus.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+Template Name: Contact us
+*/
+get_header(); ?>
+<div class="container">
+
+  <h1><?php the_title(); ?></h1>
+  <div class="row">
+    <div class="col-lg-6">
+      this is the form
+    </div>
+    <div class="col-lg-6">
+      content
+    </div>
+  </div>
+
+</div>
+
+
+<?php get_footer(); ?>
