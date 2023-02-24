e2f03fc7796e4fb572929569a12193854f415c88
Yinon bar <yinonbar1988@gmail.com>
2023-02-24 10:38:39 +0200

    menus

--- mrdigital-theme/functions.php
+++ mrdigital-theme/functions.php
@@ -2,10 +2,20 @@
 
 function loadScripts()
 {
+  wp_enqueue_script('jquery');
   wp_enqueue_style("bootstrap", "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css");
   wp_enqueue_style("mainStyle", get_theme_file_uri() . "/css/main.css");
-  wp_enqueue_script('jquery');
   wp_enqueue_script("bootstrapJS", "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js", 'jquery', false, true);
 }
 
 add_action('wp_enqueue_scripts', 'loadScripts');
+
+add_theme_support('menus');
+
+// Menus
+register_nav_menus(
+  array(
+    'top-menu' => 'Top Menu Location',
+    'mobile-menu' => 'Mobile Menu Location',
+  )
+);
