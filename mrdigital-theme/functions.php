5608feb1909927f13adbe18a42df96f56e0783e0
Yinon bar <yinonbar1988@gmail.com>
2023-02-24 09:56:39 +0200

    clean slate

--- mrdigital-theme/functions.php
+++ mrdigital-theme/functions.php
@@ -3,6 +3,8 @@
 function loadScripts()
 {
   wp_enqueue_style("bootstrap", "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css");
+  wp_enqueue_style("mainStyle", get_stylesheet_uri());
+  wp_enqueue_script("bootstrapJS", "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js", 'jquery', false, true);
 }
 
 add_action('wp_enqueue_scripts', 'loadScripts');
