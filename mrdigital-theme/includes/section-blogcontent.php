5380bb3905418a14ff68aa37857abdff85d1169c
Yinon bar <yinonbar1988@gmail.com>
2023-02-24 12:26:22 +0200

    blog

--- mrdigital-theme/includes/section-blogcontent.php
+++ mrdigital-theme/includes/section-blogcontent.php
@@ -1,6 +1,7 @@
 <?php if (have_posts()) {
   while (have_posts()) { ?>
     <?php the_post(); ?>
+    <p><?php echo get_the_date() ?></p>
     <h2><?php the_title(); ?></h2>
     <p><?php the_content(); ?></p>
 <?php the_author();
