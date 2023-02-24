25dfcc67acce5430eb83be602ddfe29631eedf28
Yinon bar <yinonbar1988@gmail.com>
2023-02-24 11:59:27 +0200

    blog

--- mrdigital-theme/archive.php
+++ mrdigital-theme/archive.php
@@ -1,8 +1,7 @@
 <?php get_header(); ?>
 <div class="page-wrap">
   <div class="container">
-
-    <?php get_template_part("includes/section", "content") ?>
+    <?php get_template_part("includes/section", "archive") ?>
   </div>
 </div>
 
---
+++ mrdigital-theme/includes/section-archive.php
@@ -0,0 +1,12 @@
+<?php if (have_posts()) {
+  while (have_posts()) { ?>
+    <div class="card mb-3">
+      <div class="card-body">
+        <?php the_post(); ?>
+        <h2><?php the_title(); ?></h2>
+        <?php the_excerpt(); ?>
+        <a href="<?php the_permalink() ?>" class="btn btn-success">Read more</a>
+      </div>
+    </div>
+<?php }
+} ?>
