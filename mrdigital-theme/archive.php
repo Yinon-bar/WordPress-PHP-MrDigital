780ce852a2e43f4debcebb9516582e94deb4f058
Yinon bar <yinonbar1988@gmail.com>
2023-02-26 10:58:40 +0200

    badges

---
+++ mrdigital-theme/archive.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+<div class="page-wrap">
+  <div class="container">
+
+    <?php get_template_part("includes/section", "archive") ?>
+  </div>
+</div>
+
+
+<?php get_footer(); ?>
--- mrdigital-theme/includes/section-blogcontent.php
+++ mrdigital-theme/includes/section-blogcontent.php
@@ -4,6 +4,20 @@
     <p><?php echo get_the_date() ?></p>
     <h2><?php the_title(); ?></h2>
     <p><?php the_content(); ?></p>
-<?php the_author();
+    <?php the_author();
+
+    $tags = get_the_tags();
+    foreach ($tags as $tag) { ?>
+      <a href="echo get_tag_link($tag->term_id);" class="badge bg-success">
+        <?php echo $tag->name ?>
+      </a>
+    <?php }
+
+    $categories = get_the_category();
+    foreach ($categories as $category) { ?>
+      <a href="<?php echo get_category_link($category->term_id); ?>" class="badge bg-primary">
+        <?php echo $category->name ?>
+      </a>
+<?php   }
   }
 } ?>
