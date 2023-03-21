79605e3a7b466c1c2cd01aff0c131cac8980acaa
Yinon Bar <63056754+Yinon-bar@users.noreply.github.com>
2023-03-21 10:05:34 +0200

    Update front-page.php

--- mrdigital-theme/front-page.php
+++ mrdigital-theme/front-page.php
@@ -2,7 +2,7 @@
 <section class="page-wrap">
   <div class="container">
 
-    <h1>test3</h1>
+    <h1>test4</h1>
     <?php get_template_part("includes/section", "content") ?>
     <?php get_search_form() ?>
   </div>
