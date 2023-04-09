15e2bd504c9df1975616323a993096de04bbc7a9
Yinon Bar <63056754+Yinon-bar@users.noreply.github.com>
2023-04-09 18:26:44 +0300

    Update front-page.php

--- mrdigital-theme/front-page.php
+++ mrdigital-theme/front-page.php
@@ -3,7 +3,7 @@
   <div class="container">
     <?php get_template_part("includes/section", "content") ?>
     <?php get_search_form() ?>
-    <h2>commit5</h2>
+    <h2>commit4</h2>
   </div>
 </section>
 
