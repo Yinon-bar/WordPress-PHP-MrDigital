50b2f20359618664fc24b32319cb4dc02c9b7b0a
Yinon Bar <63056754+Yinon-bar@users.noreply.github.com>
2023-04-19 08:41:13 +0300

    Update page.php

--- mrdigital-theme/page.php
+++ mrdigital-theme/page.php
@@ -5,7 +5,7 @@
     <?php if (has_post_thumbnail()) { ?>
       <img src="<?php the_post_thumbnail_url('blog-large') ?>" class="img-fluid mb-3 img-thumbnail">
     <?php  } ?>
-    <h1>try6</h1>
+    <h1>try5</h1>
     <?php get_template_part("includes/section", "content") ?>
   </div>
 </div>
