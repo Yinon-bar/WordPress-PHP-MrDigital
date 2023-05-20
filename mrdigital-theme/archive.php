216181edfc9b300fade73b366edacdd70687bd97
Yinon Bar <63056754+Yinon-bar@users.noreply.github.com>
2023-05-20 18:46:56 +0300

    Update archive.php

--- mrdigital-theme/archive.php
+++ mrdigital-theme/archive.php
@@ -3,7 +3,7 @@
   <div class="container">
 
     <h1><?php echo single_cat_title() ?></h1>
-
+    <h1>commit15</h1>
     <?php get_template_part("includes/section", "archive") ?>
     <?php previous_post_link() ?>
     <?php next_post_link() ?>
